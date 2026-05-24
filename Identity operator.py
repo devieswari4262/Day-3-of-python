306f1061526c70700282d5b9b5e87f8e0527c8e8
devi eswari <vanapallidevieswari@gmail.com>
2026-05-24 15:57:08 +0530

    Add files via upload

---
+++ Identity operator.py
@@ -0,0 +1,21 @@
+# Identity operator
+
+'''a=10
+b=10
+print(a is b)
+print(a is not b)
+o/p:True
+False'''
+
+
+'''a=[1,2,3]
+b=a
+print(a is b)
+print(a is not b)
+o/p:True
+False'''
+
+a=10
+b=22
+print(a is b)
+print(a is not b)
