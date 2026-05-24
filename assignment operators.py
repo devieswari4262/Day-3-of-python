45db0fd741a4020b99afb0c4721c897ed225889d
devi eswari <vanapallidevieswari@gmail.com>
2026-05-24 16:14:47 +0530

    Add files via upload

---
+++ assignment operators.py
@@ -0,0 +1,19 @@
+# assignment operation
+
+salary= int(input("Enter the salary:"))
+salary +=500
+print(salary)
+o/p:Enter the salary:15000
+15500
+ 
+salary= int(input("Enter the salary:"))
+salary *=5
+print(salary)
+o/p:Enter the salary:1000
+5000
+
+salary= int(input("Enter the salary:"))
+salary -=500
+print(salary)
+o/p:Enter the salary:15500
+15000
